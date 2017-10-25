dfe544f79d108235b582e50136fa6ff72795393c
Harrison Harnisch <hharnisc@gmail.com>
2017-10-25 18:44:29 -0500

    remove components package and consume external components package (#202)

--- packages/components/style/animation.js
+++
@@ -1,3 +0,0 @@
-export const transitionTimeMilliseconds = 200;
-export const transitionAnimationTime = `${transitionTimeMilliseconds / 1000}s`;
-export const transitionAnimationType = 'linear';
--- packages/components/style/dropShadow.js
+++
@@ -1,4 +0,0 @@
-import { outerSpaceUltraDark, outerSpaceLight } from './color';
-
-export const boxShadowLevelOne = `0 1px 2px ${outerSpaceLight}`;
-export const boxShadowLevelTwo = `0 1px 4px ${outerSpaceUltraDark}`;
