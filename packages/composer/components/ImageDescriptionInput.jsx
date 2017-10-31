fe250796ef10b7f2275c146c829c783bc0423c8b
Philippe Masset <pioul06+github@gmail.com>
2017-10-31 17:04:20 +0100

    Close image description modal after saving
    Adapted from bufferapp/buffer-web@ef076aba573b6b491a9faae925be166b2b2e5c8a

--- packages/composer/components/ImageDescriptionInput.jsx
+++ packages/composer/components/ImageDescriptionInput.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ComposerActionCreators from '../action-creators/ComposerActionCreators';
+import ModalActionCreators from '../__legacy-buffer-web-shared-components__/modal/actionCreators';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import styles from './css/ImageDescriptionInput.css';
-import ComposerActionCreators from '../action-creators/ComposerActionCreators';
 
 class ImageDescriptionInput extends React.Component {
   static propTypes = {
@@ -26,6 +27,14 @@ class ImageDescriptionInput extends React.Component {
     });
   }
 
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.saveButtonText === 'Save' && this.state.saveButtonText === 'Saved!') {
+      setTimeout(() => {
+        ModalActionCreators.closeModal();
+      }, 800);
+    }
+  };
+
   onClick = () => {
     ComposerActionCreators.updateImageDescription(this.props.mediaAttachment, this.state.description);
     this.setState({ saveButtonText: 'Saved!' });
@@ -37,7 +46,7 @@ class ImageDescriptionInput extends React.Component {
         <Input
           className={styles.textInput} type="text"
           value={this.state.description} onChange={this.handleChange}
-          placeholder="Add a description for the visually impaired"
+          placeholder="Add a description for people with visual impairments"
         />
         <Button className={styles.button} onClick={this.onClick}>
           {this.state.saveButtonText}
