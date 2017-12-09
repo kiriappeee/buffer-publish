6f8c2923ed14d4bbb18f32f71084d99abd343021
Federico Weber <federicoweber@gmail.com>
2017-12-08 18:52:24 -0800

    add performance tracking middleware and rpc

---
+++ packages/server/rpc/performanceTracking/index.js
@@ -0,0 +1,21 @@
+const { method } = require('@bufferapp/micro-rpc');
+const StatsD = require('node-dogstatsd').StatsD;
+
+const dogstatsd = new StatsD('dd-agent.default');
+
+module.exports = method(
+  'performance',
+  'track performance',
+  ({ data }) => {
+    if (!data.tags) data.tags = [];
+    data.tags.push('app:publish');
+    data.name = `buffer.perf.${data.name}`;
+
+    dogstatsd.histogram(
+      data.name,
+      data.duration,
+      1,
+      data.tags,
+    );
+    return data;
+  });
