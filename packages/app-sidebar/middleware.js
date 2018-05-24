a5d335ef9d60e7fa4473691f27d8675c8823921e
Jose M. Gilgado <jm.gilgado@gmail.com>
2018-05-24 18:29:15 +0200

    Remove Fullstory from middleware

--- packages/app-sidebar/middleware.js
+++ packages/app-sidebar/middleware.js
@@ -1,6 +1,5 @@
 import {
   actions as dataFetchActions,
-  actionTypes as dataFetchActionTypes,
 } from '@bufferapp/async-data-fetch';
 
 export default ({ dispatch }) => next => (action) => {
@@ -10,14 +9,6 @@ export default ({ dispatch }) => next => (action) => {
       dispatch(dataFetchActions.fetch({
         name: 'user',
       }));
-      break;
-    case `user_${dataFetchActionTypes.FETCH_SUCCESS}`:
-      /* eslint-disable */
-      if (FS) {
-        FS.identify(action.result.id, {
-          email: action.result.email,
-        });
-      }
       break;
     default:
       break;
