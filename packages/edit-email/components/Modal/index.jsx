04ae7ba2ecfbbdc8cdaffbf61098e08b62f654b2
Mike San Román <mike@msanroman.io>
2018-06-18 13:25:14 +0200

    Fix styling issues on edit-email/Modal

--- packages/edit-email/components/Modal/index.jsx
+++ packages/edit-email/components/Modal/index.jsx
@@ -8,19 +8,26 @@ const Modal = ({ hideModal, updateEmail, saveEmail, email }) =>
     <div style={{ width: '30rem', margin: '0 25px' }}>
       <Card>
         <div style={{ textAlign: 'center' }}>
-          <span style={{ display: 'block' }}><Text size="large" color="outerSpace">Edit your email address</Text></span>
-          <span style={{ display: 'block', margin: '.75rem 0 1rem 0' }}><Text>Your current email address is <Text bold>{email}</Text></Text></span>
+          <span style={{ display: 'block' }}>
+            <Text size="large" color="outerSpace">Edit your email address</Text>
+          </span>
+          <span style={{ display: 'block', margin: '.75rem 0 1rem 0' }}>
+            <Text>Your current email address is <Text bold>{email}</Text></Text>
+          </span>
         </div>
         <form style={{ padding: '0 2.5rem' }}>
-          <Input type="email" placeholder="Enter your email address" input={{onChange: updateEmail}} />
+          <Input type="email" placeholder="Enter your email address" input={{ onChange: updateEmail }} />
           <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', margin: '1.5rem 0' }}>
             <Button tertiary large onClick={hideModal}>
               Cancel
             </Button>
-            <Button large onClick={(e) => {
-              e.preventDefault();
-              saveEmail();
-            }}>
+            <Button
+              large
+              onClick={(e) => {
+                e.preventDefault();
+                saveEmail();
+              }}
+            >
               Save Changes
             </Button>
           </div>
@@ -32,6 +39,7 @@ const Modal = ({ hideModal, updateEmail, saveEmail, email }) =>
 Modal.propTypes = {
   hideModal: PropTypes.func.isRequired,
   updateEmail: PropTypes.func.isRequired,
+  saveEmail: PropTypes.func.isRequired,
   email: PropTypes.string.isRequired,
 };
 
