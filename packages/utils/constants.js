4b7c32d45d5f307962ad9f2d3a4d27f0eccca058
Hamish Macpherson <hamstu@gmail.com>
2018-06-22 13:41:14 -0700

    add constants

---
+++ packages/utils/constants.js
@@ -0,0 +1,28 @@
+const SERVICE_TWITTER = 'twitter';
+const SERVICE_FACEBOOK = 'facebook';
+const SERVICE_LINKEDIN = 'linkedin';
+const SERVICE_APPDOTNET = 'appdotnet';
+const SERVICE_GOOGLE = 'google';
+const SERVICE_PINTEREST = 'pinterest';
+const SERVICE_INSTAGRAM = 'instagram';
+
+const constants = {
+  SERVICE_NAMES: [
+    SERVICE_TWITTER,
+    SERVICE_FACEBOOK,
+    SERVICE_LINKEDIN,
+    SERVICE_APPDOTNET,
+    SERVICE_GOOGLE,
+    SERVICE_PINTEREST,
+    SERVICE_INSTAGRAM,
+  ],
+  SERVICE_TWITTER,
+  SERVICE_FACEBOOK,
+  SERVICE_LINKEDIN,
+  SERVICE_APPDOTNET,
+  SERVICE_GOOGLE,
+  SERVICE_PINTEREST,
+  SERVICE_INSTAGRAM,
+};
+
+export default constants;
