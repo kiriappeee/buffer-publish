ad510f4f912c679438c35962af3fee5ae34060b5
Hamish Macpherson <hamstu@gmail.com>
2018-06-25 14:24:46 -0700

    add story snapshots to testing

--- packages/two-factor-auth/components/TwoFactorAuth/story.jsx
+++ packages/two-factor-auth/components/TwoFactorAuth/story.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { action, storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
 import TwoFactorAuth from './index';
+import machine from '../../machine';
 
 const transitionAction = action('transition');
+const stories = storiesOf('TwoFactorAuth').addDecorator(checkA11y);
 
-storiesOf('TwoFactorAuth')
-  .addDecorator(checkA11y)
-  .add('disabled', () => (
+Object.keys(machine).forEach((machineState) => {
+  stories.add(machineState, () => (
     <TwoFactorAuth
-      machineState="disabled"
-      isEnabled={false}
+      machineState={machineState}
+      isEnabled={machineState === 'enabled'}
       transition={transitionAction}
     />
   ));
+});
