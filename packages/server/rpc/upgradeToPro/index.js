f144c47661e211f082323e6b65e22ae333ff957d
Mike San Román <mike@msanroman.io>
2018-06-25 15:35:13 +0200

    Add error handling for upgradeToPro

--- packages/server/rpc/upgradeToPro/index.js
+++ packages/server/rpc/upgradeToPro/index.js
@@ -1,23 +1,30 @@
-const { method } = require('@bufferapp/micro-rpc');
+const { method, createError } = require('@bufferapp/micro-rpc');
 const rp = require('request-promise');
 
 module.exports = method(
   'upgradeToPro',
   'upgrade user to the pro plan',
-  ({ cycle, token }, { session }) =>
-    rp({
-      uri: `${process.env.API_ADDR}/1/billing/start-or-upgrade-subscription.json`,
-      method: 'POST',
-      strictSSL: process.env.NODE_ENV !== 'development',
-      body: {
-        cycle,
-        stripeToken: token,
-        access_token: session.publish.accessToken,
-        product: 'publish',
-        plan: 'pro',
-      },
-    })
-    .then(result => JSON.parse(result))
-    .catch(e => console.log(e)), // eslint-disable-line no-console
+  async ({ cycle, token }, { session }) => {
+    let result;
+    try {
+      result = await rp({
+        uri: `${process.env.API_ADDR}/1/billing/start-or-upgrade-subscription.json`,
+        method: 'POST',
+        strictSSL: process.env.NODE_ENV !== 'development',
+        json: true,
+        body: {
+          cycle,
+          stripeToken: token,
+          access_token: session.publish.accessToken,
+          product: 'publish',
+          plan: 'pro',
+        },
+      });
+    } catch (response) {
+      throw createError({
+        message: response.error,
+      });
+    }
+    return result;
+  },
 );
-
