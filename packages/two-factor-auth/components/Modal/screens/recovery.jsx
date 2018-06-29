06e3abdcffb75c7123d88ae791c31fe8ec70dea4
Hamish Macpherson <hamstu@gmail.com>
2018-06-29 12:47:50 -0700

    update recovery screen

--- packages/two-factor-auth/components/Modal/screens/recovery.jsx
+++ packages/two-factor-auth/components/Modal/screens/recovery.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Text, Button } from '@bufferapp/components';
 import { AutoSelectText } from '@bufferapp/publish-shared-components';
 
 const Recovery = ({ transition, recoveryCode, handleRecoveryCodeSelect }) => (
-  <React.Fragment>
-    <Text>Save this one-time recovery code</Text>
+  <Fragment>
+    <div style={{ textAlign: 'center' }}>
+      <Text size="large">Save this one-time recovery code</Text>
+      <div style={{ margin: '12px 0 20px' }}>
+        <Text size="mini">
+          With Two Factor Authentication, if you lose your phone there&apos;s a possibility
+          you could get locked out of your account. Save this code in a safe place to use if
+          you can&apos;t log in with your phone.
+        </Text>
+      </div>
+    </div>
+
     <AutoSelectText onSelect={handleRecoveryCodeSelect} copyToClipboard>
       {recoveryCode}
     </AutoSelectText>
-    <div>
+
+    <div style={{ textAlign: 'center', marginTop: '16px' }}>
       <Button onClick={() => transition('CLOSE')}>Done</Button>
     </div>
-  </React.Fragment>
+  </Fragment>
 );
 
 Recovery.propTypes = {
