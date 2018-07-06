8447d198cafb88a05e39e29b9a279316bd498d13
anafilipadealmeida <anafilipadealmeida@gmail.com>
2018-07-06 13:25:09 +0100

    Add horizontal scroll to upgrade modal

--- packages/upgrade-modal/components/UpgradeModal/index.jsx
+++ packages/upgrade-modal/components/UpgradeModal/index.jsx
@@ -54,104 +54,106 @@ const UpgradeModal = ({
     <Popover onOverlayClick={hideModal}>
       <div style={{ maxHeight: '100vh', overflow: 'auto' }}>
         <Card>
-          <div style={{ width: '550px', margin: '0 25px' }}>
-            <div style={{ textAlign: 'center', margin: '0 0 1rem 0' }}>
-              <Text size="large" color="outerSpace">{translations.proUpgradeHeader}</Text>
-            </div>
-
-            <div style={{ display: 'flex' }}>
-              <div style={{ flex: '1' }}>
-                <ul style={listStyleLeft}>
-                  <ListItem text={translations.proPlanSocialAccounts} />
-                  <ListItem text={translations.proPlanFiltering} />
-                  <ListItem text={translations.proPlanCuration} />
-                </ul>
-              </div>
-              <div style={{ flex: '1' }}>
-                <ul style={listStyle}>
-                  <ListItem text={translations.proPlanScheduling} />
-                  <ListItem text={translations.proPlanCalendar} />
-                  <ListItem text={translations.proPlanBitly} />
-                </ul>
-              </div>
-            </div>
-
-            <Divider marginTop="" marginBottom="1.5rem" />
-
-            <PlanCycleSelect
-              translations={translations}
-              cycle={cycle}
-              selectCycle={selectCycle}
-            />
-
-            <div style={{ textAlign: 'center', margin: '1.5rem 0 1rem' }}>
-              <Text>{translations.enterPaymentDetails} <LockIcon color="shamrock" /></Text>
-            </div>
-
-            <div style={{ display: 'flex' }}>
-              <div style={{ flex: 1, paddingRight: '25px' }}>
-                <InputText id="name" label={translations.nameOnCard} store={storeValue} />
-              </div>
-              <div style={{ flex: 1 }}>
-                <InputText id="number" label={translations.cardNumber} store={storeValue} />
-              </div>
-            </div>
-
-            <div style={{ display: 'flex', marginTop: '1rem' }}>
-              <div style={{ flex: 1, paddingRight: '25px' }}>
-                <Select id="expMonth" label={translations.expirationMonth} store={storeValue}>
-                  <option />
-                  <option value="01">1 - Jan</option>
-                  <option value="02">2 - Feb</option>
-                  <option value="03">3 - Mar</option>
-                  <option value="04">4 - Apr</option>
-                  <option value="05">5 - May</option>
-                  <option value="06">6 - Jun</option>
-                  <option value="07">7 - Jul</option>
-                  <option value="08">8 - Aug</option>
-                  <option value="09">9 - Sep</option>
-                  <option value="10">10 - Oct</option>
-                  <option value="11">11 - Nov</option>
-                  <option value="12">12 - Dec</option>
-                </Select>
+          <div style={{ maxWidth:'100vw', overflow: 'auto' }}>
+            <div style={{ width: '550px', padding: '0 25px' }}>
+              <div style={{ textAlign: 'center', margin: '0 0 1rem 0' }}>
+                <Text size="large" color="outerSpace">{translations.proUpgradeHeader}</Text>
               </div>
 
-              <div style={{ flex: 1, paddingRight: '25px' }}>
-                <Select id="expYear" label={translations.expirationYear} store={storeValue}>
-                  <option />
-                  {[...Array(26).keys()].map(i => (
-                    <option
-                      key={currentYear + i}
-                      value={currentYear + i}
-                    >
-                      {currentYear + i}
-                    </option>
-                  ))}
-                </Select>
+              <div style={{ display: 'flex' }}>
+                <div style={{ flex: '1' }}>
+                  <ul style={listStyleLeft}>
+                    <ListItem text={translations.proPlanSocialAccounts} />
+                    <ListItem text={translations.proPlanFiltering} />
+                    <ListItem text={translations.proPlanCuration} />
+                  </ul>
+                </div>
+                <div style={{ flex: '1' }}>
+                  <ul style={listStyle}>
+                    <ListItem text={translations.proPlanScheduling} />
+                    <ListItem text={translations.proPlanCalendar} />
+                    <ListItem text={translations.proPlanBitly} />
+                  </ul>
+                </div>
               </div>
 
-              <div style={{ flex: 1, paddingRight: '25px', position: 'relative' }}>
-                <InputText
-                  id="cvc"
-                  label={translations.securityCode}
-                  backgroundStyle={creditCardBackground}
-                  store={storeValue}
-                />
+              <Divider marginTop="" marginBottom="1.5rem" />
+
+              <PlanCycleSelect
+                translations={translations}
+                cycle={cycle}
+                selectCycle={selectCycle}
+              />
+
+              <div style={{ textAlign: 'center', margin: '1.5rem 0 1rem' }}>
+                <Text>{translations.enterPaymentDetails} <LockIcon color="shamrock" /></Text>
               </div>
 
-              <div style={{ flex: 1 }}>
-                <InputText id="addressZip" label={translations.zipCode} note={translations.zipLeaveBlank} store={storeValue} />
+              <div style={{ display: 'flex' }}>
+                <div style={{ flex: 1, paddingRight: '25px' }}>
+                  <InputText id="name" label={translations.nameOnCard} store={storeValue} />
+                </div>
+                <div style={{ flex: 1 }}>
+                  <InputText id="number" label={translations.cardNumber} store={storeValue} />
+                </div>
               </div>
-            </div>
 
-            <div style={{ textAlign: 'center', margin: '2rem 0 0' }}>
-              <Button large disabled={validating} onClick={upgradePlan}>
-                {validating ? translations.validating : translations.upgradeCta }
-              </Button>
-              <br /><br />
-              <Button secondary large borderless onClick={hideModal}>
-                {translations.stayOnFreeCta}
-              </Button>
+              <div style={{ display: 'flex', marginTop: '1rem' }}>
+                <div style={{ flex: 1, paddingRight: '25px' }}>
+                  <Select id="expMonth" label={translations.expirationMonth} store={storeValue}>
+                    <option />
+                    <option value="01">1 - Jan</option>
+                    <option value="02">2 - Feb</option>
+                    <option value="03">3 - Mar</option>
+                    <option value="04">4 - Apr</option>
+                    <option value="05">5 - May</option>
+                    <option value="06">6 - Jun</option>
+                    <option value="07">7 - Jul</option>
+                    <option value="08">8 - Aug</option>
+                    <option value="09">9 - Sep</option>
+                    <option value="10">10 - Oct</option>
+                    <option value="11">11 - Nov</option>
+                    <option value="12">12 - Dec</option>
+                  </Select>
+                </div>
+
+                <div style={{ flex: 1, paddingRight: '25px' }}>
+                  <Select id="expYear" label={translations.expirationYear} store={storeValue}>
+                    <option />
+                    {[...Array(26).keys()].map(i => (
+                      <option
+                        key={currentYear + i}
+                        value={currentYear + i}
+                      >
+                        {currentYear + i}
+                      </option>
+                    ))}
+                  </Select>
+                </div>
+
+                <div style={{ flex: 1, paddingRight: '25px', position: 'relative' }}>
+                  <InputText
+                    id="cvc"
+                    label={translations.securityCode}
+                    backgroundStyle={creditCardBackground}
+                    store={storeValue}
+                  />
+                </div>
+
+                <div style={{ flex: 1 }}>
+                  <InputText id="addressZip" label={translations.zipCode} note={translations.zipLeaveBlank} store={storeValue} />
+                </div>
+              </div>
+
+              <div style={{ textAlign: 'center', margin: '2rem 0 0' }}>
+                <Button large disabled={validating} onClick={upgradePlan}>
+                  {validating ? translations.validating : translations.upgradeCta }
+                </Button>
+                <br /><br />
+                <Button secondary large borderless onClick={hideModal}>
+                  {translations.stayOnFreeCta}
+                </Button>
+              </div>
             </div>
           </div>
         </Card>
