151188b13b2bf83805f377710a401207aefa9fdf
Jose M. Gilgado <jm.gilgado@gmail.com>
2018-07-10 06:29:34 +0200

    Removed mocked connected apps from components

--- packages/preferences/components/AppsAndExtras/index.jsx
+++ packages/preferences/components/AppsAndExtras/index.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import ManageAppsAndExtras from '@bufferapp/manage-apps-extras';
 
-const connectedApps = [
-  {
-    id: 1,
-    name: 'Buffer client 1',
-  },
-  {
-    id: 2,
-    name: 'Buffer client 2',
-  },
-];
-
-const AppsAndExtras = () => <ManageAppsAndExtras connectedApps={connectedApps} />;
+const AppsAndExtras = () => <ManageAppsAndExtras />;
 
 export default AppsAndExtras;
