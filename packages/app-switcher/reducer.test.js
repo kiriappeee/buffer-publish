42fb16cb43296de012f183fdea958c44ca79a6f7
Maya Uribe <mariale.uribe@gmail.com>
2018-08-28 19:37:28 -0400

    Adjusting test cases for app switcher reducer.

--- packages/app-switcher/reducer.test.js
+++ packages/app-switcher/reducer.test.js
@@ -38,66 +38,47 @@ describe('reducer', () => {
         .toEqual(stateAfter);
     });
 
-    it('sendFeedback_fetchStart triggers a FETCH_START action', () => {
-      const submittingFeedback = true;
-      const redirecting = false;
-      const showGoBackToClassic = false;
-      const loading = true;
-      const result = { features: ['...'] };
+    it('sets submittingFeedback to true on FETCH_START', () => {
+      const stateAfter = {
+        ...initialState,
+        submittingFeedback: true,
+      };
 
       const action = {
         type: `sendFeedback_${dataFetchActionTypes.FETCH_START}`,
-        redirecting,
-        showGoBackToClassic,
-        submittingFeedback,
-        user: { loading },
-        result,
       };
-      const stateAfter = { redirecting, showGoBackToClassic, submittingFeedback, user: { loading } };
+
       deepFreeze(action);
       expect(reducer(undefined, action))
         .toEqual(stateAfter);
     });
 
-    it('sendFeedback_fetchSuccess triggers a FETCH_SUCCESS action', () => {
-      const submittingFeedback = false;
-      const redirecting = true;
-      const showGoBackToClassic = false;
-      const loading = true;
-      const result = { features: ['...'] };
+    it('sets submittingFeedback to false and redirecting to true on FETCH_SUCCESS', () => {
+      const stateAfter = {
+        ...initialState,
+        submittingFeedback: false,
+        redirecting: true,
+      };
 
       const action = {
         type: `sendFeedback_${dataFetchActionTypes.FETCH_SUCCESS}`,
-        redirecting,
-        showGoBackToClassic,
-        submittingFeedback,
-        user: { loading },
-        result,
       };
 
-      const stateAfter = { redirecting, showGoBackToClassic, submittingFeedback, user: { loading } };
       deepFreeze(action);
       expect(reducer(undefined, action))
         .toEqual(stateAfter);
     });
 
-    it('sendFeedback_fetchFail triggers a FETCH_FAIL action', () => {
-      const submittingFeedback = false;
-      const redirecting = false;
-      const showGoBackToClassic = false;
-      const loading = true;
-      const result = { features: ['...'] };
+    it('sets submittingFeedback to false on FETCH_FAIL', () => {
+      const stateAfter = {
+        ...initialState,
+        submittingFeedback: false,
+      };
 
       const action = {
         type: `sendFeedback_${dataFetchActionTypes.FETCH_FAIL}`,
-        redirecting,
-        showGoBackToClassic,
-        submittingFeedback,
-        user: { loading },
-        result,
       };
 
-      const stateAfter = { redirecting, showGoBackToClassic, submittingFeedback, user: { loading } };
       deepFreeze(action);
       expect(reducer(undefined, action))
         .toEqual(stateAfter);
