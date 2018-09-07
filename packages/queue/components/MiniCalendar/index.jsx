0bb94c9751bc80758cbaca6019801b9b6c5627a6
anafilipadealmeida <anafilipadealmeida@gmail.com>
2018-09-07 12:12:22 +0100

    Display number posts in calendar when change month

--- packages/queue/components/MiniCalendar/index.jsx
+++ packages/queue/components/MiniCalendar/index.jsx
@@ -11,6 +11,7 @@ import { ArrowRightIcon, ArrowLeftIcon } from '@bufferapp/components/Icon/Icons'
 import { Button } from '@bufferapp/components';
 import 'react-day-picker/lib/style.css'; // Not being used.
 import moment from 'moment-timezone';
+import { actions } from '../../reducer';
 
 const containerStyle = {
   position: 'absolute',
@@ -22,12 +23,6 @@ const containerStyle = {
   padding: '0.5rem',
   overflow: 'hidden',
   zIndex: '1',
-};
-
-const fakeDateData = {
-  'Tue Feb 06 2018': 4,
-  'Wed Feb 07 2018': 2,
-  'Thu Feb 08 2018': 1,
 };
 
 const cellStyle = {
@@ -57,6 +52,7 @@ const NavBar = ({
   onNextClick,
   className,
   localeUtils,
+  getNumberPostsByDate,
 }) => {
   const styleLeft = {
     float: 'left',
@@ -68,12 +64,18 @@ const NavBar = ({
 
   const onNext = () => {
     onNextClick();
-    // add our hooks here
+    getNumberPosts(nextMonth);
   };
 
   const onPrev = () => {
     onPreviousClick();
-    // add our hooks here
+    getNumberPosts(previousMonth);
+  };
+
+  const getNumberPosts = (newMonth) => {
+    const startDate = moment(newMonth).startOf('month').unix();
+    const endDate = moment(newMonth).endOf('month').unix();
+    getNumberPostsByDate(startDate, endDate);
   };
 
   const showPreviousButton = (moment(month).isAfter(firstMonthDisplay, 'month')) ? true : false;
@@ -96,17 +98,6 @@ const NavBar = ({
   );
 };
 
-const renderDay = (day) => {
-  const dayString = day.toDateString();
-  const numPosts = fakeDateData[dayString];
-  return (
-    <div style={cellStyle}>
-      <div style={dateStyle}>{day.getDate()}</div>
-      {numPosts && <div style={numPostsStyle}>{numPosts + ' post'}{numPosts > 1 ? 's' : ''}</div>}
-    </div>
-  )
-};
-
 const modifiers = {
   isToday: new Date(),
   disabled: {
@@ -114,16 +105,31 @@ const modifiers = {
   },
 };
 
-const MiniCalendar = () =>
-  <div style={containerStyle}>
-    <DayPicker
-      navbarElement={<NavBar/>}
-      fromMonth={firstMonthDisplay}
-      renderDay={renderDay}
-      modifiers={modifiers}
-      showOutsideDays
-    />
-  </div>;
+const MiniCalendar = ({numberPostsByDate, onMiniCalendarMonthChange}) => {
+
+  const renderDay = (day) => {
+    const dayString = day.toDateString();
+    const numPosts = numberPostsByDate && numberPostsByDate[dayString];
+    return (
+      <div style={cellStyle}>
+        <div style={dateStyle}>{day.getDate()}</div>
+        {numPosts && <div style={numPostsStyle}>{numPosts + ' post'}{numPosts > 1 ? 's' : ''}</div>}
+      </div>
+    )
+  };
+
+  return (
+    <div style={containerStyle}>
+      <DayPicker
+        navbarElement={<NavBar getNumberPostsByDate={onMiniCalendarMonthChange} />}
+        fromMonth={firstMonthDisplay}
+        renderDay={renderDay}
+        modifiers={modifiers}
+        showOutsideDays
+      />
+    </div>
+  );
+};
 
 MiniCalendar.propTypes = {
 };
