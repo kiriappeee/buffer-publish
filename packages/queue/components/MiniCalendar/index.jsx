9f8c167b1f283d76d3f6f497212b4e69114f870e
anafilipadealmeida <anafilipadealmeida@gmail.com>
2018-09-20 17:34:57 +0100

    Remove unnecessary daypicker navbar

--- packages/queue/components/MiniCalendar/index.jsx
+++ packages/queue/components/MiniCalendar/index.jsx
@@ -1,18 +1,5 @@
-/*
-This DatePicker is not importing react-day-picker styles,
-it is using DatePicker.css from the buffer-composer.
-In the future the styles should probably be independent.
-The react-day-picker styles were left here commented as
-a precaution, in case there's a change in buffer-composer
-and DatePicker.css no longer applies.
-*/
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import DayPicker from 'react-day-picker';
-import { ArrowRightIcon, ArrowLeftIcon } from '@bufferapp/components/Icon/Icons';
-import { Button } from '@bufferapp/components';
-// import 'react-day-picker/lib/style.css'; // Not being used. See comment on top.
 import moment from 'moment-timezone';
 import { InputDate } from '@bufferapp/components';
 
@@ -47,61 +34,13 @@ const numPostsStyle = {
 /* MiniCalendar displays one month in the past */
 const firstMonthToDisplay = moment().subtract(1, 'month').toDate();
 
-const NavBar = ({
-  month,
-  nextMonth,
-  previousMonth,
-  onPreviousClick,
-  onNextClick,
-  className,
-  localeUtils,
-  getNumberOfPostsByDate,
-}) => {
-  const styleLeft = {
-    float: 'left',
-  };
+const MiniCalendar = ({numberOfPostsByDate, onMonthChange}) => {
 
-  const styleRight = {
-    float: 'right',
-  };
-
-  const onNext = () => {
-    onNextClick();
-    getNumberOfPosts(nextMonth);
-  };
-
-  const onPrev = () => {
-    onPreviousClick();
-    getNumberOfPosts(previousMonth);
-  };
-
-  const getNumberOfPosts = (newMonth) => {
+  const onNavigationClick = (newMonth) => {
     const startDate = moment(newMonth).startOf('month').unix();
     const endDate = moment(newMonth).endOf('month').unix();
-    getNumberOfPostsByDate(startDate, endDate);
+    onMonthChange(startDate, endDate);
   };
-
-  const showPreviousButton = (moment(month).isAfter(firstMonthDisplay, 'month')) ? true : false;
-
-  return (
-    <div className={className}>
-      <div style={styleLeft}>
-        { showPreviousButton &&
-          <Button style={styleLeft} noStyle onClick={onPrev}>
-            <ArrowLeftIcon/>
-          </Button>
-        }
-      </div>
-      <div style={styleRight}>
-        <Button noStyle onClick={onNext}>
-          <ArrowRightIcon/>
-        </Button>
-      </div>
-    </div>
-  );
-};
-
-const MiniCalendar = ({numberOfPostsByDate, onMonthChange}) => {
 
   /* Requests the number of posts for the current month when open the calendar */
   if(!numberOfPostsByDate) {
@@ -121,12 +60,12 @@ const MiniCalendar = ({numberOfPostsByDate, onMonthChange}) => {
       </div>
     )
   };
-
   return (
     <div style={containerStyle}>
       <InputDate
         firstMonthToDisplay={firstMonthToDisplay}
         initialMonth={new Date()}
+        onNavigationClick={onNavigationClick}
       />
     </div>
   );
