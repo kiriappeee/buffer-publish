c52779ac879ba88139c72a2935ecdf3144696c6f
lolaodelola <damzcodes@gmail.com>
2018-10-23 17:34:52 +0100

    general settings test

--- packages/general-settings/index.test.jsx
+++ packages/general-settings/index.test.jsx
@@ -1,10 +1,38 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 import {
   actions,
   actionTypes,
   middleware,
 } from './index';
+import GeneralSettings from '@bufferapp/publish-general-settings';
+import InstagramDirectUpload from '@bufferapp/publish-general-settings/components/InstagramDirectUpload';
+
+const storeFake = state => ({
+  default: () => {},
+  subscribe: () => {},
+  dispatch: () => {},
+  getState: () => ({ ...state }),
+});
+
 
 describe('GeneralSettings', () => {
+  it('should render', () => {
+    const store = storeFake({
+      generalSettings: {
+        directPostingEnabled: false,
+      },
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <InstagramDirectUpload onSetUpDirectPostingClick={null} />
+      </Provider>,
+    );
+    expect(wrapper.find(InstagramDirectUpload).length)
+      .toBe(1);
+  });
+
   it('should export actions', () => {
     expect(actions)
       .toBeDefined();
